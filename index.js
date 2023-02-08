40287a2cd3bc568d2625f404dccfe9085d452000
Zbigniew Kowalkowski <zkowalkowski@gmail.com>
2023-02-08 12:37:06 +0100

    3.6: Phonebook backend step6
    Implement error handling for creating new entries

--- index.js
+++ index.js
@@ -58,11 +58,10 @@ app.delete('/api/persons/:id', (req, res) => {
 app.post('/api/persons', (req, res) => {
   const body = req.body
 
-  if (!body.name) {
-    return res.status(400).json({
-      error: 'content missing'
-    })
-  }
+  if (!body.name) {return res.status(400).json(`error: name missing`)};
+  if (!body.number) {return res.status(400).json(`error: number missing`)};
+  if (persons.find(person => person.name === body.name)) 
+    {return res.status(400).json(`error: name must be unique`)};
 
   const person = {
     id: Math.floor(Math.random()*1000),
