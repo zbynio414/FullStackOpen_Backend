0bb1185405527990d5ba48cb422fe8600281b574
Zbigniew Kowalkowski <zkowalkowski@gmail.com>
2023-02-14 12:11:32 +0100

    ValidationError message correction

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const errorHandler = (error, req, res, next) => {
   if (error.name === 'CastError') {
     return res.status(400).send({ error: 'malformated id'})
   } else if (error.name === 'ValidationError') {
-    return res.status(400).json({ error: error.message })
+    return res.status(400).json({ error: `${error.message} - validationError`})
   }
 
   next(error)
